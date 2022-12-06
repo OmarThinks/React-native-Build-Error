881ea5b28972e88cfb4e3e762d04ae8b5fe48543
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-12-06 23:59:33 +0200

    Signning data

--- AwesomeTSProject/App.tsx
+++ AwesomeTSProject/App.tsx
@@ -118,3 +118,26 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
+/*
+sudo keytool -genkey -v -keystore my-upload-key.keystore -alias my-key-alias -keyalg RSA -keysize 2048 -validity 10000
+What is your first and last name? Omar Magdy
+What is the name of your organizational unit? GitHub
+What is the name of your organization? Microsoft
+What is the name of your City or Locality? 6th of October city
+What is the name of your State or Province? Egypt
+What is the two-letter country code for this unit? EG
+
+
+MYAPP_UPLOAD_STORE_FILE=my-upload-key.keystore
+MYAPP_UPLOAD_KEY_ALIAS=my-key-alias
+MYAPP_UPLOAD_STORE_PASSWORD=abcdefghijklmnop
+MYAPP_UPLOAD_KEY_PASSWORD=abcdefghijklmnop
+
+
+
+
+LogIn: 
+harveyspectere@gmail.com
+12345
+*/
